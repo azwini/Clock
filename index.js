4571b03888144ed8f7831506c995ab007d35a513
Az <52291744+azwini@users.noreply.github.com>
2022-06-22 20:06:34 +0530

    added updated clock
    added 'for loop try

--- index.js
+++ index.js
@@ -3,9 +3,28 @@ setInterval(() => {
   hr = d.getHours();
   min = d.getMinutes();
   sec = d.getSeconds();
+  // dash = d.dashes();
   hr_rotation = 30 * hr + min / 2; //converting current time
   min_rotation = 6 * min;
   sec_rotation = 6 * sec;
+
+  // function dash() {
+  //   // Dashes
+  //   ctx.lineWidth = 3;
+  //   for (let i = 0; i < 60; i++) {
+  //     let r = 135,
+  //       l = 5;
+  //     ctx.strokeStyle = "rgba(0, 0, 0, 0.25)";
+  //     if (i % 5 === 0)
+  //       (r -= l), (l *= 2), (ctx.strokeStyle = "rgba(0, 0, 0, 0.5)");
+  //     let v = new Vector(r, Math.PI * 2 * (i / 60) - Math.PI / 2);
+  //     ctx.beginPath();
+  //     ctx.moveTo(v.getX() + c.x, v.getY() + c.y);
+  //     v.setMag(r + l);
+  //     ctx.lineTo(v.getX() + c.x, v.getY() + c.y);
+  //     ctx.stroke();
+  //   }
+  // }
 
   hour.style.transform = `rotate(${hr_rotation}deg)`;
   minute.style.transform = `rotate(${min_rotation}deg)`;
